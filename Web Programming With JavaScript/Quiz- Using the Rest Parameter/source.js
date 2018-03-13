f56b342a716f3d0b8e88e1227b1e7a96033c7761
Diana <diana.ratcheva@gmail.com>
2018-03-13 14:53:00 +0200

    Using the Rest Parameter

---
+++ Web Programming With JavaScript/Quiz- Using the Rest Parameter/source.js
@@ -0,0 +1,23 @@
+/*
+ * Programming Quiz: Using the Rest Parameter (1-5)
+ */
+
+// your code goes here
+
+function average(...nums) {
+    let sum = 0;
+    for(const num of nums){
+       sum += num;
+        
+    }
+    if (nums.length === 0){
+        return 0;
+    }else {
+    return (sum/nums.length);
+    }
+}
+
+console.log(average(2, 6));
+console.log(average(2, 3, 3, 5, 7, 10));
+console.log(average(7, 1432, 12, 13, 100));
+console.log(average());
